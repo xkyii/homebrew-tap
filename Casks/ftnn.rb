ef09b3274d1969c9d2919395467f58196fe4e2d8
vkyii <vkyii@foxmail.com>
2016-04-07 11:36:05 +0800

    *Add ftnn

---
+++ Casks/ftnn.rb
@@ -0,0 +1,11 @@
+cask 'ftnn' do
+  version '3.8.2'
+  sha256 :no_check
+
+  url 'https://www.futunn.com/client/nn/mac/FTNNForMac_3.8.2_2016032403_std_release_std_officially.dmg'
+  name 'FTNN'
+  homepage 'https://www.futunn.com'
+  license :unknown # TODO: change license and remove this comment; ':unknown' is a machine-generated placeholder
+
+  app 'FTNN.app'
+end
