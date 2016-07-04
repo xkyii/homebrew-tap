e1b31597550b3e37312ae2c8faf235228515edd2
vkyii <vkyii@foxmail.com>
2016-07-04 09:06:06 +0800

    *Add AppCleaner

---
+++ Casks/appcleaner.rb
@@ -0,0 +1,11 @@
+cask 'appcleaner' do
+  version '3.3'
+  sha256 :no_check
+
+  url 'http://7xsxgj.com1.z0.glb.clouddn.com/free/AppCleaner_3.3.zip'
+  name 'AppCleaner'
+  homepage ''
+  license :unknown
+
+  app 'AppCleaner.app'
+end
