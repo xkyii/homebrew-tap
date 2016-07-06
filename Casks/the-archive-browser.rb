544c4b91f72ffe65b1ddd9c30c3c537d4a90a284
vkyii <vkyii@foxmail.com>
2016-07-06 11:22:50 +0800

    *Add the archive browser

---
+++ Casks/the-archive-browser.rb
@@ -0,0 +1,11 @@
+cask 'the-archive-browser' do
+  version '1.10.1'
+  sha256 :no_check
+
+  url 'http://7xsxgj.com1.z0.glb.clouddn.com/crack/The%20Archive%20Browser.zip'
+  name 'The Archive Browser'
+  homepage ''
+  license :unknown
+
+  app 'The Archive Browser.app'
+end
